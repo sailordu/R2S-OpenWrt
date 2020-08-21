d6358fed0310b46813ab8157e71a820385a1d78c
msylgj <msylgj@vip.qq.com>
2020-08-21 11:41:15 +0800

    test crypto

--- SCRIPTS/prepare_package.sh
+++ SCRIPTS/prepare_package.sh
@@ -3,7 +3,9 @@ clear
 ##准备工作
 #Kernel
 wget -O- https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/3277.patch | patch -p1
-wget -O- https://github.com/project-openwrt/openwrt/commit/abb0ba46c021595d49c35609b70e473e6c79d127.patch | patch -p1
+
+#Crypto（test
+wget -O- https://github.com/AmadeusGhost/lede/commit/3e668936669080ca6f3fcea5534b94d00103291a.patch | patch -p1
 
 #使用19.07的feed源
 rm -f ./feeds.conf.default
